e9f95ccb4f2b9ab12dfa879c2ee99cf4581be63c
Gymnasiast <m.o.steenbeek@gmail.com>
2015-04-21 11:58:50 +0200

    Viewport, voor mobiele apparaten

--- pagina.php
+++ pagina.php
@@ -61,6 +61,7 @@ class Pagina
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="nl">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title><?php echo $this->paginanaam . ' - ' . $websitenaam . '</title>'; 
 		echo '<link href="stijl.css" type="text/css" rel="stylesheet" />';
 		if($favicon=geefInstelling('favicon'))
