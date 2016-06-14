acc518ae3e7c6d11ad02a542c2758c5048c8f361
Michael Steenbeek <m.o.steenbeek@gmail.com>
2016-06-14 14:55:24 +0200

    Migratiescript voor 4.1

---
+++ migreer_naar_v4.1.php
@@ -0,0 +1,8 @@
+<?php
+require('check.php');
+require_once('functies.db.php');
+
+geefEen('ALTER TABLE subs MODIFY tekst MEDIUMTEXT',array());
+geefEen('ALTER TABLE vorigesubs MODIFY tekst MEDIUMTEXT',array());
+
+echo 'Upgrade voltooid.';
