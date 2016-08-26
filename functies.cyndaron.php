dcc9a6064c4a71968d798b8864504b5c491a1835
Michael Steenbeek <m.o.steenbeek@gmail.com>
2016-08-26 12:30:28 +0200

    Volgende versie wordt 5.0, niet 4.1
    Teveel wijzigingen voor een tussenversie

--- functies.cyndaron.php
+++ functies.cyndaron.php
@@ -2,17 +2,17 @@
 function geefCyndaronVersie()
 {
     // Dit is de versie van de Cyndaron Engine.
-    return '4.0.98.0';
+    return '4.9.98.0';
 }
 
 function geefProductCodenaam()
 {
-    return 'Ostia';
+    return 'Scupi';
 }
 
 function geefProductVersie()
 {
-    return '4.1 alpha 0';
+    return '5.0 alpha 0';
 }
 
 function geefProductNaam()
