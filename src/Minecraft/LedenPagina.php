4ac04f204a69b013f5da13b7256ffb8ba0dffbd2
Gymnasiast <m.o.steenbeek@gmail.com>
2017-03-22 23:01:42 +0100

    Toon mc-leden in actievere pose, schoon CSS op

--- src/Minecraft/LedenPagina.php
+++ src/Minecraft/LedenPagina.php
@@ -30,31 +30,31 @@ class LedenPagina extends Pagina
         foreach ($spelers as $speler)
         {
             if ($speler['niveau'] >= 3 && $laatsteniveau == 0)
-                echo '<h2 style="border-bottom: 1px dotted;">Politbureau</h2>';
+                echo '<h2>Politbureau</h2>';
             if ($speler['niveau'] == 2 && $laatsteniveau >= 3)
-                echo '<h2 style="border-bottom: 1px dotted;">Leden</h2>';
+                echo '<h2>Leden</h2>';
             if ($speler['niveau'] == 1 && $laatsteniveau >= 2)
-                echo '<h2 style="border-bottom: 1px dotted;">Aspirant-leden</h2>';
+                echo '<h2>Aspirant-leden</h2>';
             if ($speler['niveau'] == 0 && $laatsteniveau >= 1)
-                echo '<h2 style="border-bottom: 1px dotted;">In de Goelag</h2>';
+                echo '<h2>In de Goelag</h2>';
             $laatsteniveau = $speler['niveau'];
-            $vooraanzicht = "mc-skinrenderer?vr=-10&hr=20&hrh=0&vrla=0&vrra=0&vrll=0&vrrl=0&ratio=4&format=png&displayHair=false&headOnly=false&user={$speler['mcnaam']}";
-            $achteraanzicht = "mc-skinrenderer?vr=-10&hr=200&hrh=0&vrla=0&vrra=0&vrll=0&vrrl=0&ratio=4&format=png&displayHair=false&headOnly=false&user={$speler['mcnaam']}";
+            $vooraanzicht = "mc-skinrenderer?vr=-10&hr=20&hrh=0&vrla=-20&vrra=20&vrll=15&vrrl=-10&ratio=4&format=png&displayHair=false&headOnly=false&user={$speler['mcnaam']}";
+            $achteraanzicht = "mc-skinrenderer?vr=-10&hr=200&hrh=0&vrla=-20&vrra=20&vrll=15&vrrl=-10&ratio=4&format=png&displayHair=false&headOnly=false&user={$speler['mcnaam']}";
             $tePreloaden[] = $achteraanzicht;
 
-            echo '<div style="display: inline-block; overflow:hidden;">';
+            echo '<div class="spelerswrapper">';
             echo '<table>';
-            echo '<tr><td style="width: 100px; padding: 10px 30px 10px 30px;">';
+            echo '<tr><td class="avatarbox">';
 
             echo '<img alt="Avatar van ' . $speler['echtenaam'] . '" title="Avatar van ' . $speler['echtenaam'] . '" src="' . $vooraanzicht . '" onmouseover="this.src=\'' . $achteraanzicht . '\'" onmouseout="this.src=\'' . $vooraanzicht . '\'" />';
             echo '</td>';
-            echo '<td  style="width: 350px; padding: 10px 10px 10px 10px; vertical-align: middle;">';
+            echo '<td class="spelersinfobox">';
 
-            echo '<span style="font-family: Trebuchet MS, Arial, sans-serif; font-size: 40px;">' . $speler['mcnaam'] . '</span>';
+            echo '<span class="spelersnaam">' . $speler['mcnaam'] . '</span>';
 
             if ($speler['donateur'] == 1)
             {
-                echo '<br /><span style="font-weight: bold; color: #B8860B;">Donateur</span>';
+                echo '<br /><span class="donateur">Donateur</span>';
             }
 
             echo '<br />Echte naam: ' . $speler['echtenaam'];
@@ -70,15 +70,52 @@ class LedenPagina extends Pagina
             echo '</tr>';
             echo '</table>';
             echo '</div>';
-
-            echo '<style type="text/css">body:after { content:';
-
-            foreach ($tePreloaden as $image)
-            {
-                printf('url(%s) ', $image);
-            }
-            echo '; display: none;</style>';
         }
+        $preloadUrls = '';
+
+        foreach ($tePreloaden as $image)
+        {
+            $preloadUrls .= sprintf('url(%s) ', $image);
+        }
+        ?>
+        <style type="text/css">
+            h2
+            {
+                border-bottom: 1px dotted;
+            }
+            .spelerswrapper
+            {
+                display: inline-block;
+                overflow:hidden;
+            }
+            .spelersnaam
+            {
+                font-family: "Trebuchet MS", Arial, sans-serif;
+                font-size: 40px;
+            }
+            .avatarbox
+            {
+                width: 100px;
+                padding: 10px 30px 10px 30px;
+            }
+            .spelersinfobox
+            {
+                width: 350px;
+                padding: 10px 10px 10px 10px;
+                vertical-align: middle;
+            }
+            .donateur
+            {
+                font-weight: bold;
+                color: #B8860B;
+            }
+            body:after
+            {
+                content: <?=$preloadUrls;?>;
+                display: none;
+            }
+        </style>
+        <?php
 
         $this->toonPostPagina();
     }
