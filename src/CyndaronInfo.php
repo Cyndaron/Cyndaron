cbb3199ecaf214be78a58bfe3acbd5787adb02b1
Gymnasiast <m.o.steenbeek@gmail.com>
2017-03-23 20:26:26 +0100

    Verhoog versienummer naar 5.0 bèta 1

--- src/CyndaronInfo.php
+++ src/CyndaronInfo.php
@@ -3,8 +3,8 @@ namespace Cyndaron;
 
 class CyndaronInfo
 {
-    const ENGINE_VERSIE = '4.9.99.0';
+    const ENGINE_VERSIE = '4.9.99.1';
     const PRODUCT_CODENAAM = 'Scupi';
-    const PRODUCT_VERSIE = '5.0 beta 0';
+    const PRODUCT_VERSIE = '5.0 bèta 1';
     const PRODUCT_NAAM = 'Cyndaron';
 }
