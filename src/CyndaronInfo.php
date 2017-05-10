4d05b2e4d26a98b924cab13cad7cce87e910fcd1
Gymnasiast <m.o.steenbeek@gmail.com>
2017-05-10 23:22:38 +0200

    Release 5.0

--- src/CyndaronInfo.php
+++ src/CyndaronInfo.php
@@ -3,8 +3,8 @@ namespace Cyndaron;
 
 class CyndaronInfo
 {
-    const ENGINE_VERSIE = '4.9.99.3';
+    const ENGINE_VERSIE = '5.0';
     const PRODUCT_CODENAAM = 'Scupi';
-    const PRODUCT_VERSIE = '5.0 bèta 3';
+    const PRODUCT_VERSIE = '5.0';
     const PRODUCT_NAAM = 'Cyndaron';
 }
