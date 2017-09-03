826a34c9cdffc251a5473435defff8a10a3dd0f5
Gymnasiast <m.o.steenbeek@gmail.com>
2017-09-03 16:01:05 +0200

    Werk versie-informatie bij

--- src/CyndaronInfo.php
+++ src/CyndaronInfo.php
@@ -3,8 +3,8 @@ namespace Cyndaron;
 
 class CyndaronInfo
 {
-    const ENGINE_VERSIE = '5.1.99.1';
+    const ENGINE_VERSIE = '5.2.0';
     const PRODUCT_CODENAAM = 'Stobi';
-    const PRODUCT_VERSIE = '5.2.0 bèta 1';
+    const PRODUCT_VERSIE = '5.2';
     const PRODUCT_NAAM = 'Cyndaron';
 }
