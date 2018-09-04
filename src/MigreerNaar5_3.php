411d28efac363fd8a428500e85284f4d73463a0d
Gymnasiast <m.o.steenbeek@gmail.com>
2018-09-04 20:08:34 +0200

    Fix migratiescript

--- src/MigreerNaar5_3.php
+++ src/MigreerNaar5_3.php
@@ -3,14 +3,14 @@ namespace Cyndaron;
 
 require __DIR__ . '/../check.php';
 
-class MigreerNaar5_0 extends Pagina
+class MigreerNaar5_3 extends Pagina
 {
     public function __construct()
     {
         $connectie = DBConnection::getInstance();
         $connectie->doQuery('ALTER TABLE `gebruikers` ADD `email` VARCHAR(255) NULL DEFAULT NULL AFTER `wachtwoord`;');
 
-        parent::__construct('Upgrade naar versie 5.0');
+        parent::__construct('Upgrade naar versie 5.3');
         $this->toonPrepagina();
         echo 'De upgrade is voltooid.';
         $this->toonPostPagina();
