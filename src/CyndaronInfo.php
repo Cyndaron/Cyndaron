5837f18040826328a7ecc169ada080fb39488979
Gymnasiast <m.o.steenbeek@gmail.com>
2019-01-04 20:17:10 +0100

    5.3 bèta 1

--- src/CyndaronInfo.php
+++ src/CyndaronInfo.php
@@ -3,8 +3,8 @@ namespace Cyndaron;
 
 class CyndaronInfo
 {
-    const ENGINE_VERSIE = '5.2.99.0';
+    const ENGINE_VERSIE = '5.2.99.1';
     const PRODUCT_CODENAAM = 'Lychnidus';
-    const PRODUCT_VERSIE = '5.3 bèta 0';
+    const PRODUCT_VERSIE = '5.3 bèta 1';
     const PRODUCT_NAAM = 'Cyndaron';
 }
