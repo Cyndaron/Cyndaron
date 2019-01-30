5d5e4dd06c27c000e5a5b2bd239391e74827e266
Gymnasiast <m.o.steenbeek@gmail.com>
2019-01-30 22:51:13 +0100

    Meer styling paginaoverzicht

--- src/PageManager/PageManagerPage.php
+++ src/PageManager/PageManagerPage.php
@@ -76,60 +76,80 @@ class PageManagerPage extends Pagina
 
         foreach ($subsPerCategorie as $categorie => $subs)
         {
-            echo '<h3 class="text-italic">' . $categorie . '</h3>';
-            echo '<table class="table table-striped table-bordered table-overzicht">';
-
-            foreach ($subs as $id => $name)
-            {
-                $vvsub = $this->connectie->prepare('SELECT * FROM vorigesubs WHERE id= ?');
-                $vvsub->execute([$id]);
-                $hasLastVersion = boolval($vvsub->fetchColumn());
-                $name = strtr($name, [' ' => '&nbsp;']);
-                ?>
-                <tr id="pm-row-sub-<?=$id?>">
-                    <td>
-                        <div class="btn-group">
-                            <?= new Knop('edit', '/editor-statischepagina?id=' . $id, 'Bewerk deze statische pagina', null, 16);?>
-                            <button class="btn btn-outline-cyndaron btn-sm pm-delete" data-type="sub" data-id="<?=$id;?>" ><span class="glyphicon glyphicon-trash"></span></button>
-                            <button class="btn btn-outline-cyndaron btn-sm pm-addtomenu" data-type="sub" data-id="<?=$id;?>" ><span class="glyphicon glyphicon-bookmark"></span></button>
-                            <?php if ($hasLastVersion)
-                            {
-                                echo new Knop('lastversion', '/editor-statischepagina?vorigeversie=1&amp;id=' . $id, 'Vorige versie terugzetten', null, 16);
-                            }
-                            ?>
-                        </div>
-                    </td>
-                    <td>
-                        <span style="font-size: 15px;">
-                            <a href="/toonsub.php?id=<?=$id?>"><b><?=$name?></b></a>
-                        </span>
-                    </td>
-                </tr>
+            ?>
+            <h3 class="text-italic"><?=$categorie?></h3>
+            <table class="table table-striped table-bordered pm-table">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Naam</th>
+                        <th>Acties</th>
+                    </tr>
+                </thead>
+                <tbody>
                 <?php
-            }
-            echo '</table>';
+                foreach ($subs as $id => $name):
+
+                    $vvsub = $this->connectie->prepare('SELECT * FROM vorigesubs WHERE id= ?');
+                    $vvsub->execute([$id]);
+                    $hasLastVersion = boolval($vvsub->fetchColumn());
+                    $name = strtr($name, [' ' => '&nbsp;']);
+                    ?>
+                    <tr id="pm-row-sub-<?=$id?>">
+                        <td><?=$id?></td>
+                        <td>
+                            <span style="font-size: 15px;">
+                                <a href="/toonsub.php?id=<?=$id?>"><b><?=$name?></b></a>
+                            </span>
+                        </td>
+                        <td>
+                            <div class="btn-group">
+                                <?= new Knop('edit', '/editor-statischepagina?id=' . $id, 'Bewerk deze statische pagina', null, 16);?>
+                                <button class="btn btn-outline-cyndaron btn-sm pm-delete" data-type="sub" data-id="<?=$id;?>" ><span class="glyphicon glyphicon-trash"></span></button>
+                                <button class="btn btn-outline-cyndaron btn-sm pm-addtomenu" data-type="sub" data-id="<?=$id;?>" ><span class="glyphicon glyphicon-bookmark"></span></button>
+                                <?php if ($hasLastVersion)
+                                {
+                                    echo new Knop('lastversion', '/editor-statischepagina?vorigeversie=1&amp;id=' . $id, 'Vorige versie terugzetten', null, 16);
+                                }
+                                ?>
+                            </div>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+                </tbody>
+            </table><?php
         }
     }
 
     public function showCategories()
     {
         ?>
-        <!-- Categorieën -->
         <h2>Categorieën</h2>
-            <div class="form-group">
-                <label for="pm-category-new-name">Nieuwe categorie: </label>
-                <input class="form-control" id="pm-category-new-name" type="text"/>
-            </div>
-            <button id="pm-create-category" class="btn btn-success"><span class="glyphicon glyphicon-plus"></span> Aanmaken
-            </button>
-        <br/>
-        <table class="table table-striped table-bordered table-overzicht"><?php
+
+        <div class="form-inline">
+            <label for="pm-category-new-name" class="mb-2 mr-sm-2">Nieuwe categorie:</label>
+            <input class="form-control mb-2 mr-sm-2" id="pm-category-new-name" type="text"/>
+            <button id="pm-create-category" class="btn btn-success mb-2 "><span class="glyphicon glyphicon-plus"></span> Aanmaken</button>
+        </div>
+
+        <table class="table table-striped table-bordered pm-table">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Naam</th>
+                    <th>Acties</th>
+                </tr>
+            </thead>
+            <tbody>
+            <?php
             $categories = $this->connectie->prepare('SELECT id,naam FROM categorieen ORDER BY id ASC;');
             $categories->execute();
-            foreach ($categories as $category)
-            {
-                ?>
+            foreach ($categories as $category): ?>
                 <tr id="pm-row-category-<?=$category['id']?>">
+                    <td><?=$category['id']?></td>
+                    <td>
+                        <a href="/tooncategorie.php?id=<?php echo $category['id']; ?>"><b><?php echo $category['naam']; ?></b></a>
+                    </td>
                     <td>
                         <div class="btn-group"><?php
                             echo new Knop('edit', '/editor-categorie?id=' . $category['id'], 'Deze categorie bewerken', null, 16);?>
@@ -137,11 +157,9 @@ class PageManagerPage extends Pagina
                             <button class="btn btn-outline-cyndaron btn-sm pm-addtomenu" data-type="category" data-id="<?=$category['id'];?>" ><span class="glyphicon glyphicon-bookmark" title="Voeg deze categorie toe aan het menu"></span></button>
                         </div>
                     </td>
-                    <td>
-                        <a href="/tooncategorie.php?id=<?php echo $category['id']; ?>"><b><?php echo $category['naam']; ?></b></a>
-                    </td>
                 </tr>
-            <?php } ?>
+            <?php endforeach; ?>
+            </tbody>
         </table>
         <?php
     }
@@ -149,37 +167,42 @@ class PageManagerPage extends Pagina
     public function showPhotoAlbums()
     {
         ?>
-        <!-- Fotoboeken -->
-        <h2>Fotoboeken</h2>
-        <form method="post" action="/photoalbum/new" class="form-inline">
-            <div class="form-group">
-                <label for="pm-photoalbum-new-name">Nieuw fotoboek:</label>
-                <input id="pm-photoalbum-new-name" type="text" class="form-control"/>
-                <button id="pm-create-photoalbum" class="btn btn-success"><span class="glyphicon glyphicon-plus"></span> Aanmaken
-                </button>
-            </div>
-        </form>
-        <br/>
-        <table class="table table-striped table-bordered table-overzicht"><?php
+        <h2>Fotoalbums</h2>
+
+        <div class="form-inline">
+            <label for="pm-photoalbum-new-name" class="mb-2 mr-sm-2">Nieuw fotoalbum:</label>
+            <input class="form-control mb-2 mr-sm-2" id="pm-photoalbum-new-name" type="text"/>
+            <button id="pm-create-photoalbum" class="btn btn-success mb-2 "><span class="glyphicon glyphicon-plus"></span> Aanmaken</button>
+        </div>
+
+        <table class="table table-striped table-bordered pm-table">
+            <thead>
+            <tr>
+                <th>ID</th>
+                <th>Naam</th>
+                <th>Acties</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php
             $photoalbums = $this->connectie->prepare('SELECT id,naam FROM fotoboeken ORDER BY id ASC;');
             $photoalbums->execute();
-            foreach ($photoalbums as $photoalbum)
-            {
-                ?>
+            foreach ($photoalbums as $photoalbum): ?>
                 <tr id="pm-row-photoalbum-<?=$photoalbum['id']?>">
+                    <td><?=$photoalbum['id']?></td>
+                    <td>
+                        <a href="/toonfotoboek.php?id=<?php echo $photoalbum['id']; ?>"><b><?php echo $photoalbum['naam']; ?></b></a>
+                    </td>
                     <td>
                         <div class="btn-group"><?php
-                            echo new Knop('edit', '/editor-fotoalbum?id=' . $photoalbum['id'], 'Bewerk dit fotoboek', null, 16); ?>
+                            echo new Knop('edit', '/editor-fotoalbum?id=' . $photoalbum['id'], 'Bewerk dit fotoalbum', null, 16); ?>
                             <button class="btn btn-outline-cyndaron btn-sm pm-delete" data-type="photoalbum" data-id="<?=$photoalbum['id'];?>" ><span class="glyphicon glyphicon-trash" title="Verwijder dit fotoalbum"></span></button>
                             <button class="btn btn-outline-cyndaron btn-sm pm-addtomenu" data-type="photoalbum" data-id="<?=$photoalbum['id'];?>" ><span class="glyphicon glyphicon-bookmark" title="Voeg dit fotoalbum toe aan het menu"></span></button>
                         </div
                     </td>
-                    <td>
-                        <a href="/toonfotoboek.php?id=<?php echo $photoalbum['id']; ?>"><b><?php echo $photoalbum['naam']; ?></b></a>
-                        (mapnummer <?php echo $photoalbum['id']; ?>)
-                    </td>
                 </tr>
-            <?php } ?>
+            <?php endforeach; ?>
+            </tbody>
         </table><br/>
         <?php
     }
@@ -189,42 +212,55 @@ class PageManagerPage extends Pagina
         ?>
         <h2>Friendly URL's</h2>
 
-            <table class="table table-striped table-bordered table-overzicht">
+            <table class="table table-striped table-bordered pm-table">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>URL</th>
+                        <th>Verwijzingsdoel</th>
+                        <th>Acties</th>
+                    </tr>
+                </thead>
+                <tbody>
                 <tr>
-                    <th></th>
-                    <th>URL</th>
-                    <th>Verwijzingsdoel</th>
-                </tr>
-                <tr>
-                    <td>Nieuwe friendly URL:</td>
+                    <td></td>
                     <td>
+                        Nieuwe friendly URL:
                         <input id="pm-friendlyurl-new-name" type="text" placeholder="URL"
                                class="form-control form-control-inline"/></td>
                     <td>
                         <input id="pm-friendlyurl-new-target" type="text" placeholder="Verwijzingsdoel"
                                class="form-control form-control-inline"/>
+                    </td>
+                    <td>
                         <button id="pm-create-friendlyurl" class="btn btn-success"><span class="glyphicon glyphicon-plus"></span>
                             Aanmaken
                         </button>
                     </td>
                 </tr>
                 <?php
-                $friendlyurls = $this->connectie->prepare('SELECT naam,doel FROM friendlyurls ORDER BY naam ASC;');
+                $friendlyurls = $this->connectie->prepare('SELECT * FROM friendlyurls ORDER BY naam ASC;');
                 $friendlyurls->execute();
+                $counter = 1;
 
                 foreach ($friendlyurls as $friendlyurl): ?>
                     <tr id="pm-row-friendlyurl-<?=$friendlyurl['naam'];?>">
+                        <td><?=$counter++;?></td>
+                        <td>
+                            <strong><?=$friendlyurl['naam'];?></strong>
+                        </td>
+                        <td>
+                            <?=$friendlyurl['doel']?>
+                        </td>
                         <td>
                             <div class="btn-group">
                                 <button class="btn btn-outline-cyndaron btn-sm pm-delete" data-type="friendlyurl" data-id="<?=$friendlyurl['naam'];?>" ><span class="glyphicon glyphicon-trash" title="Verwijder deze friendly URL"></span></button>
                                 <button class="btn btn-outline-cyndaron btn-sm pm-addtomenu" data-type="friendlyurl" data-id="<?=$friendlyurl['naam'];?>" ><span class="glyphicon glyphicon-bookmark" title="Voeg deze friendly URL toe aan het menu"></span></button>
                             </div>
                         </td>
-                        <td>
-                            <strong><?=$friendlyurl['naam'];?></strong></td><td><?=$friendlyurl['doel']?>
-                        </td>
                     </tr>
                 <?php endforeach;?>
+                </tbody>
             </table>
         </form>
         <?php
