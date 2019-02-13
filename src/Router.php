df5c9ccc2c797eb4019ddb2e5d1bdfc964dca78e
Gymnasiast <m.o.steenbeek@gmail.com>
2019-02-13 18:32:44 +0100

    Refactor Router

--- src/Router.php
+++ src/Router.php
@@ -1,4 +1,6 @@
 <?php
+declare (strict_types = 1);
+
 namespace Cyndaron;
 
 use Cyndaron\User\User;
@@ -9,6 +11,9 @@ use Cyndaron\User\User;
  */
 class Router
 {
+    private $request = '';
+    private $requestVars = [''];
+
     protected $endpoints = [
         // Standaard
         '403.php' => '\Cyndaron\Error403Pagina',
@@ -53,94 +58,83 @@ class Router
 
     public function __construct()
     {
-        $request = Request::geefGetVeilig('pagina') ?: '/';
-        $requestVars = (explode('/', $request));
-        Request::setVars($requestVars);
-
-        if ((substr($request, 0, 1) == '.' || substr($request, 0, 1) == '/') && $request != '/')
-        {
-            new Error403Pagina();
-            die('Deze locatie mag niet worden opgevraagd.');
-        }
-
-        $scriptSrc = "'self'";
-
-        // De CKeditor heeft helaas nog inline scripting nodig. Op deze manier voorkomen we dat de hele site
-        // daaronder moet lijden.
-        if (strpos($request, 'editor-') === 0)
-        {
-            $scriptSrc .= " 'unsafe-inline'";
-        }
-
-        if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] == "off")
-        {
-            $redirect = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-            header('HTTP/1.1 301 Moved Permanently');
-            header('Location: ' . $redirect);
-            exit();
-        }
-
-        header("Content-Security-Policy: upgrade-insecure-requests; script-src $scriptSrc; font-src 'self'; base-uri 'none'; object-src 'none'");
-
-        $hoofdurl = new Url(DBConnection::geefEen('SELECT link FROM menu WHERE volgorde=(SELECT MIN(volgorde) FROM menu)', []));
-        if ($hoofdurl->isGelijkAan(new Url($request)))
-        {
-            header('Location: /');
-        }
-
-        // Verwijs oude URLs door
-        if ($url = DBConnection::geefEen('SELECT naam FROM friendlyurls WHERE doel=?', [basename(substr($_SERVER['REQUEST_URI'], 1))]))
-        {
-            header('Location: ' . $url);
-        }
-
         if (empty($_SESSION))
         {
             session_start();
         }
 
-        //Hoofdpagina
-        if ($request == '/')
+        $request = Request::geefGetVeilig('page') ?: '/';
+        $this->updateRequestVars($request);
+
+        $this->blockPathTraversal($request);
+
+        $this->sendCSPHeader();
+
+        $this->redirectOldUrls($request);
+
+        // Frontpage
+        if ($this->requestVars[0] === '')
         {
-            $this->verwerkUrl($hoofdurl, '/');
+            $frontpage = $this->getFrontpageUrl();
+            $this->updateRequestVars((string)$frontpage);
         }
-        elseif (array_key_exists($requestVars[0], $this->endpoints))
+        // Known friendly URL
+        elseif ($url = DBConnection::geefEen('SELECT doel FROM friendlyurls WHERE naam=?', [$this->requestVars[0]]))
         {
-            $this->routeFoundNowCheckLogin($requestVars[0]);
-            $classname = $this->endpoints[$requestVars[0]];
-            if (is_subclass_of($classname, Controller::class))
-            {
-                /** @var Controller $route */
-                $route = new $classname($requestVars[0], $requestVars[1] ?? '');
-                $token = Request::geefPostVeilig('csrfToken');
-                $route->checkCSRFToken($token);
-                $route->route();
-            }
-            else
-            {
-                new $classname();
-            }
+            $this->updateRequestVars($this->rewriteFriendlyUrl(new Url($url)));
         }
 
-        // Bekende friendly URL
-        elseif ($url = new Url(DBConnection::geefEen('SELECT doel FROM friendlyurls WHERE naam=?', [$requestVars[0]])))
+        if (array_key_exists($this->requestVars[0], $this->endpoints))
         {
-            $this->verwerkUrl($url, $requestVars[0]);
+            $this->routeEndpoint();
         }
-        // Oude directe link naar een foto
-        elseif ($request === 'toonfoto.php')
-        {
-            $boekid = Request::geefGetVeilig('boekid');
-            header('Location: toonfotoboek.php?id=' . $boekid);
-        }
-        //Niet gevonden
         else
         {
             new Error404Pagina();
         }
     }
 
-    public function verwerkUrl(Url $url, string $request)
+    private function routeFoundNowCheckLogin()
+    {
+        $userLevel = User::getLevel();
+        if (!User::hasSufficientReadLevel() && $this->requestVars[0] !== 'login')
+        {
+            Request::sendDoNotCache();
+            if ($userLevel > 0)
+            {
+                new Error403Pagina();
+                die('Deze pagina mag niet worden opgevraagd.');
+            }
+            else
+            {
+                User::addNotification('U moet inloggen om deze site te bekijken');
+                $_SESSION['redirect'] = $_SERVER['REQUEST_URI'];
+                header('Location: /login');
+                die();
+            }
+        }
+    }
+
+    private function routeEndpoint(): void
+    {
+        $this->routeFoundNowCheckLogin();
+        $classname = $this->endpoints[$this->requestVars[0]];
+        if (is_subclass_of($classname, Controller::class)) {
+            /** @var Controller $route */
+            $route = new $classname($this->requestVars[0], $this->requestVars[1] ?? '');
+            $token = Request::geefPostVeilig('csrfToken');
+            $route->checkCSRFToken($token);
+            $route->route();
+        } else {
+            new $classname();
+        }
+    }
+
+    /**
+     * @param Url $url
+     * @return string
+     */
+    private function rewriteFriendlyUrl(Url $url): string
     {
         $ufUrl = $url->geefUnfriendly();
         if (strpos($ufUrl, '?') !== false)
@@ -158,41 +152,75 @@ class Router
         {
             $bestand = $ufUrl;
         }
+        return $bestand;
+    }
 
-        if (array_key_exists($bestand, $this->endpoints))
+    /**
+     * @param string $request
+     */
+    private function redirectOldUrls(string $request): void
+    {
+        $frontPage = $this->getFrontpageUrl();
+        if ($frontPage->isGelijkAan(new Url($_SERVER['REQUEST_URI'])))
         {
-            $this->routeFoundNowCheckLogin($request);
-            $classname = $this->endpoints[$bestand];
-            new $classname();
+            header('Location: /');
+            die();
         }
-        elseif (file_exists($bestand))
+        // Redirect if a friendly url exists for the requested unfriendly url
+        if ($url = DBConnection::geefEen('SELECT naam FROM friendlyurls WHERE doel LIKE ?', [$_SERVER['REQUEST_URI'] . '%']))
         {
-            include $bestand;
+            header('Location: ' . $url);
+            die();
         }
-        else
+        // Old link to photo in album (pre-Lightbox)
+        if ($request === 'toonfoto.php')
         {
-            new Error404Pagina();
+            $albumId = Request::geefGetVeilig('boekid');
+            header('Location: /photoalbum/' . $albumId);
+            die();
         }
     }
 
-    public function routeFoundNowCheckLogin($request)
+    /**
+     * @return Url
+     */
+    private function getFrontpageUrl(): Url
     {
-        $userLevel = User::getLevel();
-        if (!User::hasSufficientReadLevel() && strpos($request, 'login') !== 0)
+        $frontPage = new Url(DBConnection::geefEen('SELECT link FROM menu WHERE volgorde=(SELECT MIN(volgorde) FROM menu)', []));
+        return $frontPage;
+    }
+
+    private function sendCSPHeader(): void
+    {
+        $scriptSrc = "'self'";
+
+        // Unfortunately, CKeditor still needs inline scripting. Only allow this on editor pages,
+        // in order to prevent degrading the security of the rest of the system.
+        if (strpos($this->requestVars[0], 'editor-') === 0)
         {
-            Request::sendDoNotCache();
-            if ($userLevel > 0)
-            {
-                new Error403Pagina();
-                die('Deze pagina mag niet worden opgevraagd.');
-            }
-            else
-            {
-                User::addNotification('U moet inloggen om deze site te bekijken');
-                $_SESSION['redirect'] = $_SERVER['REQUEST_URI'];
-                header('Location: /login');
-                die();
-            }
+            $scriptSrc .= " 'unsafe-inline'";
         }
+
+        header("Content-Security-Policy: upgrade-insecure-requests; script-src $scriptSrc; font-src 'self'; base-uri 'none'; object-src 'none'");
+    }
+
+    /**
+     * @param string $request
+     */
+    private function blockPathTraversal(string $request): void
+    {
+        if ((substr($request, 0, 1) == '.' || substr($request, 0, 1) == '/') && $request != '/') {
+            new Error403Pagina();
+            die('Deze locatie mag niet worden opgevraagd.');
+        }
+    }
+
+    /**
+     * @param string $request
+     */
+    private function updateRequestVars(string $request): void
+    {
+        $this->requestVars = (explode('/', $request));
+        Request::setVars($this->requestVars);
     }
 }
