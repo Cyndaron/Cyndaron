a1c7eda6a24a2fb768069ff6f8140d5230a4e021
Michael Steenbeek <m.o.steenbeek@gmail.com>
2019-07-27 11:13:50 +0200

    Fix alignment of migration sql

--- src/Migrate60.sql
+++ src/Migrate60.sql
@@ -8,17 +8,17 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `registration_events` (
-                                       `id` int(11) NOT NULL,
-                                       `name` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
-                                       `openForRegistration` tinyint(1) NOT NULL,
-                                       `description` mediumtext COLLATE utf8mb4_unicode_ci NOT NULL,
-                                       `descriptionWhenClosed` mediumtext COLLATE utf8mb4_unicode_ci NOT NULL,
-                                       `registrationCost` double NOT NULL DEFAULT '0',
-                                       `forcedDelivery` tinyint(1) NOT NULL DEFAULT '0',
-                                       `maxRegistrations` int(11) NOT NULL DEFAULT '250',
-                                       `numSeats` int(11) NOT NULL DEFAULT '250',
-                                       `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                                       `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    `id` int(11) NOT NULL,
+    `name` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `openForRegistration` tinyint(1) NOT NULL,
+    `description` mediumtext COLLATE utf8mb4_unicode_ci NOT NULL,
+    `descriptionWhenClosed` mediumtext COLLATE utf8mb4_unicode_ci NOT NULL,
+    `registrationCost` double NOT NULL DEFAULT '0',
+    `forcedDelivery` tinyint(1) NOT NULL DEFAULT '0',
+    `maxRegistrations` int(11) NOT NULL DEFAULT '250',
+    `numSeats` int(11) NOT NULL DEFAULT '250',
+    `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -28,22 +28,22 @@ CREATE TABLE `registration_events` (
 --
 
 CREATE TABLE `registration_orders` (
-                                       `id` int(11) NOT NULL,
-                                       `eventId` int(11) NOT NULL,
-                                       `lastName` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
-                                       `initials` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
-                                       `vocalRange` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-                                       `lunch` tinyint(1) NOT NULL DEFAULT '0',
-                                       `email` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
-                                       `street` varchar(300) COLLATE utf8mb4_unicode_ci NOT NULL,
-                                       `houseNumber` int(11) NOT NULL DEFAULT '0',
-                                       `houseNumberAddition` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
-                                       `postcode` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
-                                       `city` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
-                                       `isPaid` tinyint(1) NOT NULL DEFAULT '0',
-                                       `comments` varchar(400) COLLATE utf8mb4_unicode_ci NOT NULL,
-                                       `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                                       `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    `id` int(11) NOT NULL,
+    `eventId` int(11) NOT NULL,
+    `lastName` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `initials` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `vocalRange` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `lunch` tinyint(1) NOT NULL DEFAULT '0',
+    `email` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `street` varchar(300) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `houseNumber` int(11) NOT NULL DEFAULT '0',
+    `houseNumberAddition` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+    `postcode` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `city` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `isPaid` tinyint(1) NOT NULL DEFAULT '0',
+    `comments` varchar(400) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -53,9 +53,9 @@ CREATE TABLE `registration_orders` (
 --
 
 CREATE TABLE `registration_orders_tickettypes` (
-                                                   `orderId` int(11) NOT NULL,
-                                                   `tickettypeId` int(11) NOT NULL,
-                                                   `amount` int(2) NOT NULL
+    `orderId` int(11) NOT NULL,
+    `tickettypeId` int(11) NOT NULL,
+    `amount` int(2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -65,10 +65,10 @@ CREATE TABLE `registration_orders_tickettypes` (
 --
 
 CREATE TABLE `registration_tickettypes` (
-                                            `id` int(11) NOT NULL,
-                                            `eventId` int(11) NOT NULL,
-                                            `name` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
-                                            `price` double NOT NULL
+    `id` int(11) NOT NULL,
+    `eventId` int(11) NOT NULL,
+    `name` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `price` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
