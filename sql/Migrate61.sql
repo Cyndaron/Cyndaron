357d3424800d74e43fc1b0587dba5f103e5b9071
Michael Steenbeek <m.o.steenbeek@gmail.com>
2020-05-27 13:58:56 +0200

    Create migration SQL for 6.1/7.0

---
+++ sql/Migrate61.sql
@@ -0,0 +1,21 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+CREATE TABLE `user_rights` (
+                               `userId` int NOT NULL,
+                               `right` varchar(63) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+ALTER TABLE `user_rights`
+    ADD UNIQUE KEY `userId` (`userId`,`right`);
+
+ALTER TABLE `user_rights`
+    ADD CONSTRAINT `user_rights_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
