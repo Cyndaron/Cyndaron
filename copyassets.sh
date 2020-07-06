371e2424708576628322e583b12d5e4ecd7d0437
Michael Steenbeek <m.o.steenbeek@gmail.com>
2020-07-06 12:27:37 +0200

    Copy the whole CKeditor to public_html

--- copyassets.sh
+++ copyassets.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 FILES=$(grep -IriEoh "\/(src|vendor)\/.*\.(js|css|map|png|jpg|jpeg|gif|html)" src/ | uniq)
 
+rm -Rf public_html/asset/*
+
 for FILE in $FILES
 do
    DIRNAME=$(dirname "$FILE")
    mkdir -p public_html/asset"$DIRNAME"
    cp ."$FILE" public_html/asset"$FILE"
 done
+
+mkdir -p public_html/asset/vendor/ckeditor
+cp -R vendor/ckeditor/ckeditor public_html/asset/vendor/ckeditor
