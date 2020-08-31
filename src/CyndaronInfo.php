71d95b07d68ad3dac2590fdc2e62c0f8e7fac5c6
Michael Steenbeek <m.o.steenbeek@gmail.com>
2020-08-31 20:42:01 +0200

    Bump version numbers

--- src/CyndaronInfo.php
+++ src/CyndaronInfo.php
@@ -3,8 +3,8 @@ namespace Cyndaron;
 
 final class CyndaronInfo
 {
-    public const ENGINE_VERSION = '6.0.0';
+    public const ENGINE_VERSION = '6.0.98.0';
     public const PRODUCT_CODENAME = 'Trajectum';
-    public const PRODUCT_VERSION = '6.0';
+    public const PRODUCT_VERSION = '6.1 alpha 0';
     public const PRODUCT_NAME = 'Cyndaron';
 }
