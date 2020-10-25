a2f66c41057f2c8f05be871394d59cc485f49234
Michael Steenbeek <m.o.steenbeek@gmail.com>
2020-10-25 22:18:32 +0100

    Reservation: update another notification

--- src/Geelhoed/Reservation/templates/ReserveStep1Page.blade.php
+++ src/Geelhoed/Reservation/templates/ReserveStep1Page.blade.php
@@ -1,9 +1,7 @@
 @extends('Index')
 
 @section('contents')
-    <div class="alert alert-warning">
-        Enkel voor judoka's van 18 jaar en ouder!
-    </div>
+    Voor het inschrijven voor de anderhalvemeterlessen.
 
     <div class="alert alert-info">
         Kies een les. Op sommige locaties kunnen we niet schuiven met de lessen. Dit komt meestal door de beschikbare tijd en de groepsgroottes. Wanneer uw gebruikelijke leslocatie er niet tussen staat, willen wij u vragen op een andere locatie te trainen.
