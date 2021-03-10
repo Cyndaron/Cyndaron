4b7de3d0332cdaff9e41f92b70755fdd8ec9fd10
Michael Steenbeek <m.o.steenbeek@gmail.com>
2021-03-10 14:54:44 +0100

    Add extra fields to Number

--- src/Widget/Form/templates/Number.blade.php
+++ src/Widget/Form/templates/Number.blade.php
@@ -1,1 +1,1 @@
-@include('Widget/Form/BasicInput', ['inputType' => 'number', 'id' => $id, 'label' => $label, 'value' => $value])
+@include('Widget/Form/BasicInput', ['inputType' => 'number', 'id' => $id, 'label' => $label, 'value' => $value ?? '', 'min' => $min ?? 0, 'max' => $max ?? '', 'step' => $step ?? 1])
