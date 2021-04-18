ad1bedfb2af035b49253eb7ab20131999b575e34
Michael Steenbeek <m.o.steenbeek@gmail.com>
2021-04-18 13:54:27 +0200

    Add missing field

--- src/Registration/sql/Migrate61.sql
+++ src/Registration/sql/Migrate61.sql
@@ -1,1 +1,2 @@
+ALTER TABLE `registration_events` ADD `requireApproval` TINYINT(1) NOT NULL AFTER `numSeats`;
 ALTER TABLE `registration_events` ADD `hideRegistrationFee` TINYINT(1) NOT NULL AFTER `requireApproval`;
