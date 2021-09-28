6c0b8d81defbc1c266e9ec9e049cd995e28236ba
Michael Steenbeek <m.o.steenbeek@gmail.com>
2021-09-28 22:13:32 +0200

    Sort parent accounts list by last name

--- src/Geelhoed/Contest/ParentAccountsPage.php
+++ src/Geelhoed/Contest/ParentAccountsPage.php
@@ -5,6 +5,7 @@ namespace Cyndaron\Geelhoed\Contest;
 
 use Cyndaron\View\Page;
 use Cyndaron\User\User;
+use function Safe\usort;
 
 final class ParentAccountsPage extends Page
 {
@@ -13,6 +14,10 @@ final class ParentAccountsPage extends Page
         parent::__construct('Lijst ouderaccounts');
 
         $users = User::fetchAll(['id IN (SELECT `userId` FROM `user_rights` WHERE `right` = ?)'], [Contest::RIGHT_PARENT]);
+        usort($users, static function(User $user1, User $user2)
+        {
+            return $user1->lastName <=> $user2->lastName;
+        });
         $this->addScript('/src/Geelhoed/Contest/js/ParentAccountsManager.js');
         $this->addTemplateVars([
             'users' => $users,
