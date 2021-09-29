56eac32c03ebdb1e31fa7e2a5078f57aa3221574
Michael Steenbeek <m.o.steenbeek@gmail.com>
2021-09-29 18:29:03 +0200

    Migrate61: user_rights is already created in 6.0

--- sql/Migrate61.sql
+++ sql/Migrate61.sql
@@ -8,18 +8,6 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
-CREATE TABLE `user_rights` (
-                               `userId` int NOT NULL,
-                               `right` varchar(63) COLLATE utf8mb4_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-ALTER TABLE `user_rights`
-    ADD UNIQUE KEY `userId` (`userId`,`right`);
-
-ALTER TABLE `user_rights`
-    ADD CONSTRAINT `user_rights_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-COMMIT;
-
 ALTER TABLE `categories` ADD `image` VARCHAR(100) NOT NULL AFTER `name`, ADD `blurb` VARCHAR(400) NOT NULL AFTER `image`;
 ALTER TABLE `subs` ADD `image` VARCHAR(100) NOT NULL AFTER `name`, ADD `blurb` VARCHAR(400) NOT NULL AFTER `image`;
 ALTER TABLE `photoalbums` ADD `image` VARCHAR(100) NOT NULL AFTER `name`, ADD `blurb` VARCHAR(400) NOT NULL AFTER `image`;
