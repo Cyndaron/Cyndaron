cc469801a27ab1cf3b1b11fd7b1efa57ddb1505b
Michael Steenbeek <m.o.steenbeek@gmail.com>
2021-09-29 18:41:50 +0200

    Bump version for 6.1

--- src/CyndaronInfo.php
+++ src/CyndaronInfo.php
@@ -3,8 +3,8 @@ namespace Cyndaron;
 
 final class CyndaronInfo
 {
-    public const ENGINE_VERSION = '6.0.98.0';
-    public const PRODUCT_CODENAME = 'Trajectum';
-    public const PRODUCT_VERSION = '6.1 alpha 0';
+    public const ENGINE_VERSION = '6.0.98.1';
+    public const PRODUCT_CODENAME = 'Noviomagus';
+    public const PRODUCT_VERSION = '6.1 alpha 1';
     public const PRODUCT_NAME = 'Cyndaron';
 }
