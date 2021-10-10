de6127a08caaf80b34aa84d9da4dec9a5758f964
Michael Steenbeek <m.o.steenbeek@gmail.com>
2021-10-10 14:30:23 +0200

    Fix SQL

--- src/Ticketsale/sql/Migrate61.sql
+++ src/Ticketsale/sql/Migrate61.sql
@@ -1,3 +1,3 @@
 ALTER TABLE `ticketsale_concerts` ADD `deliveryCostInterface` VARCHAR(255) NOT NULL DEFAULT '' AFTER `numReservedSeats`;
-ALTER TABLE cyndaron.`ticketsale_tickettypes` ADD `created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP AFTER `price`, ADD `modified` TIMESTAMP on update CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP AFTER `created`;
+ALTER TABLE `ticketsale_tickettypes` ADD `created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP AFTER `price`, ADD `modified` TIMESTAMP on update CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP AFTER `created`;
 ALTER TABLE `ticketsale_orders` ADD `additionalData` TEXT NOT NULL AFTER `comments`;
