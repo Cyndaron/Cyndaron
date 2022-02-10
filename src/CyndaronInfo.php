6add4970b16eaf7e3b6d918aebf4ad6142a11348
Michael Steenbeek <m.o.steenbeek@gmail.com>
2022-02-10 23:24:40 +0100

    Bump version number to 6.1 bèta

--- src/CyndaronInfo.php
+++ src/CyndaronInfo.php
@@ -3,8 +3,8 @@ namespace Cyndaron;
 
 final class CyndaronInfo
 {
-    public const ENGINE_VERSION = '6.0.98.1';
+    public const ENGINE_VERSION = '6.0.99.1';
     public const PRODUCT_CODENAME = 'Noviomagus';
-    public const PRODUCT_VERSION = '6.1 alpha 1';
+    public const PRODUCT_VERSION = '6.1 bèta 1';
     public const PRODUCT_NAME = 'Cyndaron';
 }
