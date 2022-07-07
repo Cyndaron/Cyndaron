e83d44bab1643a016f5cff0f4183aba29292c0a3
Michael Steenbeek <m.o.steenbeek@gmail.com>
2022-07-07 18:02:00 +0200

    Add html

--- src/Ticketsale/Order/templates/CheckIn.blade.php
+++ src/Ticketsale/Order/templates/CheckIn.blade.php
@@ -1,6 +1,13 @@
 @php /** @var \Cyndaron\Ticketsale\Concert $concert */ @endphp
 @php /** @var \Cyndaron\Ticketsale\Order\Order $order */ @endphp
-
+<!DOCTYPE HTML>
+<html lang="nl" class="cyndaron">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Scannen</title>
+</head>
+<body>
 @if ($message)
     <div style="color: {{ $isPositive ? 'green' : 'red' }}">{{ $message }}</div>
 @endif
@@ -9,3 +16,5 @@
     <input id="barbode" type="text" name="barcode" placeholder="barcode">
     <button type="submit">Checken</button>
 </form>
+
+</body>
