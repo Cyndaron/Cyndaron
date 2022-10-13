8a17cb8c81b487b4ee4b202b2397a4d5886be6a8
Michael Steenbeek <m.o.steenbeek@gmail.com>
2022-10-13 21:51:26 +0200

    Bump to beta 3

--- src/CyndaronInfo.php
+++ src/CyndaronInfo.php
@@ -3,8 +3,8 @@ namespace Cyndaron;
 
 final class CyndaronInfo
 {
-    public const ENGINE_VERSION = '6.0.99.2';
+    public const ENGINE_VERSION = '6.0.99.3';
     public const PRODUCT_CODENAME = 'Noviomagus';
-    public const PRODUCT_VERSION = '6.1 bèta 2';
+    public const PRODUCT_VERSION = '6.1 bèta 3';
     public const PRODUCT_NAME = 'Cyndaron';
 }
