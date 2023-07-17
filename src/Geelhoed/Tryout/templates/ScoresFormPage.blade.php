b9fa1c26ad8407398234c78bf4d68b8c0d6e68ef
Michael Steenbeek <m.o.steenbeek@gmail.com>
2023-07-18 00:46:10 +0200

    Tryout: Add some additional info to scores form page

--- src/Geelhoed/Tryout/templates/ScoresFormPage.blade.php
+++ src/Geelhoed/Tryout/templates/ScoresFormPage.blade.php
@@ -1,5 +1,14 @@
 @extends('Index')
 
 @section('contents')
+    <p>
+        U kunt hier de scores van het Tryout-toernooi opvragen door het viercijferige nummer op de deelnemerskaart in te vullen.
+        Om privacyredenen tonen wij niet welke namen aan welke nummers gekoppeld zijn.
+    </p>
+    <p>
+        Komt het aantal punten niet overeen met het kaartje,
+        dan kunt u dit op het volgende toernooi laten rechtzetten bij de hoofdjury.
+    </p>
+
     @include('Geelhoed/Tryout/ScoresForm')
 @endsection
