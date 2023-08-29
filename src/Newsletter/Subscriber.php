4bf117aa6a3150c09ec75cd473c9bd627d9e9465
Michael Steenbeek <m.o.steenbeek@gmail.com>
2023-08-29 22:43:33 +0200

    Newsletter: fix missing field

--- src/Newsletter/Subscriber.php
+++ src/Newsletter/Subscriber.php
@@ -13,7 +13,7 @@ use Cyndaron\DBAL\Model;
 class Subscriber extends Model
 {
     public const TABLE = 'newsletter_subscriber';
-    public const TABLE_FIELDS = ['name', 'email'];
+    public const TABLE_FIELDS = ['name', 'email', 'confirmed'];
 
     public string $name = '';
     public string $email = '';
