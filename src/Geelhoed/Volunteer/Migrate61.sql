d7caca942cdabf2400ed5a9c49f598d043623ca8
Michael Steenbeek <m.o.steenbeek@gmail.com>
2023-12-15 14:05:59 +0100

    Update ToT SQL

--- src/Geelhoed/Volunteer/Migrate61.sql
+++ src/Geelhoed/Volunteer/Migrate61.sql
@@ -10,17 +10,17 @@ create table geelhoed_volunteer_event
     modified datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP
 );
 
-create index geelhoed_volunteer_event_end
+create index geelhoed_volunteer_tot_end
     on geelhoed_volunteer_tot (end);
 
-create index geelhoed_volunteer_event_start
+create index geelhoed_volunteer_tot_start
     on geelhoed_volunteer_tot (start);
 
-create index geelhoed_volunteer_event_start_end
+create index geelhoed_volunteer_tot_start_end
     on geelhoed_volunteer_tot (start, end);
 
 
-create table geelhoed_volunteer_event_participation
+create table geelhoed_volunteer_tot_participation
 (
     id       int auto_increment
         primary key,
@@ -32,19 +32,22 @@ create table geelhoed_volunteer_event_participation
     comments text                               not null,
     created  datetime default CURRENT_TIMESTAMP not null,
     modified datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP,
-    constraint geelhoed_volunteer_event_participation_id_fk
+    constraint geelhoed_volunteer_tot_participation_id_fk
         foreign key (eventId) references geelhoed_volunteer_tot (id)
             on update cascade on delete cascade
 );
 
-create index geelhoed_volunteer_event_participation_email
+create index geelhoed_volunteer_tot_participation_email
     on geelhoed_volunteer_tot_participation (email);
 
-create index geelhoed_volunteer_event_participation_et
+create index geelhoed_volunteer_tot_participation_et
     on geelhoed_volunteer_tot_participation (eventId, type);
 
-create index geelhoed_volunteer_event_participation_eventId
+create index geelhoed_volunteer_tot_participation_eventId
     on geelhoed_volunteer_tot_participation (eventId);
 
 alter table geelhoed_volunteer_tot_participation
     add phone varchar(30) not null after email;
+
+alter table geelhoed_volunteer_tot
+    add photoalbumLink varchar(255) not null after data;
