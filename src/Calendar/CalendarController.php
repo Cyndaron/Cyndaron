5922e20ba555706a922ed29e83c9f3707f69bad9
Michael Steenbeek <m.o.steenbeek@gmail.com>
2024-03-31 01:55:24 +0100

    Port CalendarController to new routing system

--- src/Calendar/CalendarController.php
+++ src/Calendar/CalendarController.php
@@ -4,17 +4,15 @@ declare(strict_types=1);
 namespace Cyndaron\Calendar;
 
 use Cyndaron\Base\ModuleRegistry;
-use Cyndaron\Page\PageRenderer;
+use Cyndaron\Request\RequestMethod;
 use Cyndaron\Routing\Controller;
+use Cyndaron\Routing\RouteAttribute;
 use Cyndaron\User\UserLevel;
 use Symfony\Component\HttpFoundation\Response;
 
 class CalendarController extends Controller
 {
-    public array $getRoutes = [
-        'overzicht' => ['function' => 'overview', 'level' => UserLevel::ANONYMOUS],
-    ];
-
+    #[RouteAttribute('overzicht', RequestMethod::GET, UserLevel::ANONYMOUS)]
     public function overview(ModuleRegistry $moduleRegistry): Response
     {
         $page = new CalendarIndexPage($moduleRegistry);
