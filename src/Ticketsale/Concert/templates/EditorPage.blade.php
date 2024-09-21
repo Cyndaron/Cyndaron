e7cfbff165fa4dbb0440c4650d83c6b026a7a722
Michael Steenbeek <m.o.steenbeek@gmail.com>
2024-09-21 18:07:55 +0200

    Ticketsale: group reserved seat options together, add editor for ticketInfo

--- src/Ticketsale/Concert/templates/EditorPage.blade.php
+++ src/Ticketsale/Concert/templates/EditorPage.blade.php
@@ -5,10 +5,11 @@
 
     @include('View/Widget/Form/Checkbox', ['id' => 'openForSales', 'label' => 'Open voor verkoop', 'checked' => (bool)($model->openForSales ?? false)])
 
-    <div class="form-group">
-        <label>Beschijving indien gesloten:</label>
-        @include('View/Widget/Form/Editor', ['id' => 'descriptionWhenClosed', 'value' => $descriptionWhenClosed])
-    </div>
+    @component('View/Widget/Form/FormWrapper', ['id' => 'descriptionWhenClosed', 'label' => 'Beschijving indien gesloten'])
+        @slot('right')
+            @include('View/Widget/Form/Editor', ['id' => 'descriptionWhenClosed', 'value' => $descriptionWhenClosed])
+        @endslot
+    @endcomponent
 
     @include('View/Widget/Form/Select', ['id' => 'delivery', 'label' => 'Kaartlevering', 'required' => true, 'selected' => $delivery, 'options' => [
         0 => 'Bij kerk of opsturen',
@@ -16,23 +17,30 @@
         2 => 'Digitaal',
     ]])
 
-    @include('View/Widget/Form/Checkbox', ['id' => 'hasReservedSeats', 'label' => 'Heeft gereserveerde plaatsen', 'checked' => (bool)($model->hasReservedSeats ?? false)])
-
-    @include('View/Widget/Form/Checkbox', ['id' => 'reservedSeatsAreSoldOut', 'label' => 'Gereserveerde plaatsen zijn uitverkocht', 'checked' => (bool)($model->reservedSeatsAreSoldOut ?? false)])
-
     @include('View/Widget/Form/Select', ['id' => 'deliveryCostInterface', 'label' => 'Berekening verzendkosten', 'selected' => $deliveryCostInterface, 'options' => $deliveryCostOptions])
 
     @include('View/Widget/Form/Currency', ['id' => 'deliveryCost', 'label' => 'Verzendkosten', 'value' => $deliveryCost])
 
-    @include('View/Widget/Form/Currency', ['id' => 'reservedSeatCharge', 'label' => 'Toeslag gereserveerde plaats', 'value' => $reservedSeatCharge])
-
     @include('View/Widget/Form/Number', ['id' => 'numFreeSeats', 'label' => 'Aantal vrije plaatsen', 'value' => $numFreeSeats])
-
-    @include('View/Widget/Form/Number', ['id' => 'numReservedSeats', 'label' => 'Aantal gereserveerde plaatsen', 'value' => $numReservedSeats])
 
     @include('View/Widget/Form/BasicInput', ['id' => 'date', 'type' => 'datetime-local', 'label' => 'Datum', 'value' => $model->date ?? ''])
 
     @include('View/Widget/Form/BasicInput', ['id' => 'location', 'type' => 'text', 'label' => 'Locatie', 'value' => $model->location ?? ''])
 
-    @include('View/Widget/Form/Textarea', ['id' => 'ticketInfo', 'label' => 'Informatie op de tickets', 'value' => $model->ticketInfo ?? ''])
+    @component('View/Widget/Form/FormWrapper', ['id' => 'ticketInfo', 'label' => 'Informatie op de tickets'])
+        @slot('right')
+            @include('View/Widget/Form/Editor', ['id' => 'ticketInfo', 'value' => $model->ticketInfo ?? ''])
+        @endslot
+    @endcomponent
+
+    <hr>
+
+    @include('View/Widget/Form/Checkbox', ['id' => 'hasReservedSeats', 'label' => 'Heeft gereserveerde plaatsen', 'checked' => (bool)($model->hasReservedSeats ?? false)])
+
+    @include('View/Widget/Form/Checkbox', ['id' => 'reservedSeatsAreSoldOut', 'label' => 'Gereserveerde plaatsen zijn uitverkocht', 'checked' => (bool)($model->reservedSeatsAreSoldOut ?? false)])
+
+    @include('View/Widget/Form/Number', ['id' => 'numReservedSeats', 'label' => 'Aantal gereserveerde plaatsen', 'value' => $numReservedSeats])
+
+    @include('View/Widget/Form/Currency', ['id' => 'reservedSeatCharge', 'label' => 'Toeslag gereserveerde plaats', 'value' => $reservedSeatCharge])
+
 @endsection
