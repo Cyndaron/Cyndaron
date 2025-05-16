e1cc72ce8eb5341f0b8e2cf3cc3216a6714e3935
Michael Steenbeek <m.o.steenbeek@gmail.com>
2025-05-16 23:00:40 +0200

    Tighten form validation for OpenRCT2 servertest

--- src/OpenRCT2/ServerTest/templates/FormPage.blade.php
+++ src/OpenRCT2/ServerTest/templates/FormPage.blade.php
@@ -2,8 +2,8 @@
 
 @section('contents')
     <form method="post">
-        @include('View/Widget/Form/BasicInput', ['id' => 'ip', 'label' => 'IP', 'placeholder' => '', 'value' => ''])
-        @include('View/Widget/Form/BasicInput', ['id' => 'port', 'label' => 'Port', 'placeholder' => 'Default: 11753', 'value' => '11753'])
+        @include('View/Widget/Form/BasicInput', ['id' => 'ip', 'label' => 'IP', 'required' => true, 'placeholder' => '', 'value' => ''])
+        @include('View/Widget/Form/Number', ['id' => 'port', 'label' => 'Port', 'required' => true, 'placeholder' => 'Default: 11753', 'value' => '11753', 'min' => 1, 'max' => 65535, 'step' => 1, 'pattern' => '[0-9]+'])
         <input type="hidden" name="csrfToken" value="{{ $tokenHandler->get('servertest', '') }}"/>
 
         @component('View/Widget/Form/FormWrapper', [])
