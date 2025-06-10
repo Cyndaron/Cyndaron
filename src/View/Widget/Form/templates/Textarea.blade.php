faf860caadc64cb71b0013a9ca2c6f7bcc1d6258
Michael Steenbeek <m.o.steenbeek@gmail.com>
2025-06-10 12:49:56 +0200

    Allow specifying maxlength attribute on textarea

--- src/View/Widget/Form/templates/Textarea.blade.php
+++ src/View/Widget/Form/templates/Textarea.blade.php
@@ -1,5 +1,12 @@
 @component('View/Widget/Form/FormWrapper', ['id' => $id, 'label' => $label])
     @slot('right')
-        <textarea class="form-control" id="{{ $id }}" name="{{ $id }}" rows="4" @if (isset($placeholder)) placeholder="{{ $placeholder }}" @endif>{{ $value ?? '' }}</textarea>
+        <textarea
+            class="form-control"
+            id="{{ $id }}"
+            name="{{ $id }}"
+            rows="4"
+            @if (isset($placeholder)) placeholder="{{ $placeholder }}" @endif
+            @if (isset($maxLength)) maxlength="{{ $maxLength }}" @endif
+        >{{ $value ?? '' }}</textarea>
     @endslot
 @endcomponent
