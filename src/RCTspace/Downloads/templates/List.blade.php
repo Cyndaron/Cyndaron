be7f1d74ecd3c61d61b9bfba696c926247ee2f3b
Michael Steenbeek <m.o.steenbeek@gmail.com>
2025-08-03 12:56:46 +0200

    Small fixes to RCTspace Downloads module

--- src/RCTspace/Downloads/templates/List.blade.php
+++ src/RCTspace/Downloads/templates/List.blade.php
@@ -1,7 +1,7 @@
 @extends ('Index')
 
 @section ('contents')
-    <table>
+    <table class="table table-striped table-bordered">
         <thead>
         <tr>
             <th>ID</th>
@@ -24,12 +24,12 @@
                     $name .= " {$row['file_version']}";
             @endphp
             <tr>
-                <td class="align-right">{{ $loop->index }}</td>
+                <td class="align-right">{{ $loop->iteration }}</td>
                 <td><a href='https://forums.rctspace.com/index.php?app=downloads&showfile={{ $fileId }}' target='_blank'>{{ $name }}</a></td>
                 <td>{{ $category }}</td>
                 <td>{{ $submitter }}</td>
                 <td>{{ $submitDate }}</td>
-                <td><a href="?action=download&id={{ $fileId }}" target='_blank'>Download</a>
+                <td><a href="/downloads/download/{{ $fileId }}" target='_blank'>Download</a>
             </tr>
         @endforeach
         </tbody>
