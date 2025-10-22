34c15e0a9ece6cf0185e0260464ffb67181d03f1
Michael Steenbeek <m.o.steenbeek@gmail.com>
2025-10-22 19:57:35 +0200

    Registration: update migration file

--- src/Registration/sql/Migrate71.sql
+++ src/Registration/sql/Migrate71.sql
@@ -1,5 +1,8 @@
 alter table registration_orders_tickettypes
-    modify id int auto_increment PRIMARY KEY;
+    add id int auto_increment PRIMARY KEY first;
 
 alter table registration_orders_tickettypes
     auto_increment = 1;
+
+alter table registration_orders
+    add masterclass tinyint(1) default 0 not null after bhv;
