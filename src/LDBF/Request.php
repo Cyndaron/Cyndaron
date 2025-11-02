c9ec064df3f8fbebc7abf1e0ab62d6443fa589d5
Michael Steenbeek <m.o.steenbeek@gmail.com>
2025-11-02 13:23:39 +0100

    LDBF: set DatabaseField attributes in Request

--- src/LDBF/Request.php
+++ src/LDBF/Request.php
@@ -3,14 +3,19 @@ declare(strict_types=1);
 
 namespace Cyndaron\LDBF;
 
+use Cyndaron\DBAL\DatabaseField;
 use Cyndaron\DBAL\Model;
 
 final class Request extends Model
 {
     public const TABLE = 'ldbf_mailform_request';
 
+    #[DatabaseField]
     public string $secretCode = '';
+    #[DatabaseField]
     public string $email = '';
+    #[DatabaseField]
     public string $mailBody = '';
+    #[DatabaseField]
     public bool $confirmed = false;
 }
