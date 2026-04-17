c6bf876722a69a979a65ed23a1ed4fb85ee6da4a
Michael Steenbeek <m.o.steenbeek@gmail.com>
2026-04-17 22:10:42 +0200

    Mark IncompleteData as UserSafeError

--- src/Util/Error/IncompleteData.php
+++ src/Util/Error/IncompleteData.php
@@ -1,8 +1,9 @@
 <?php
 namespace Cyndaron\Util\Error;
 
+use Cyndaron\Util\UserSafeError;
 use RuntimeException;
 
-final class IncompleteData extends RuntimeException
+final class IncompleteData extends RuntimeException implements UserSafeError
 {
 }
