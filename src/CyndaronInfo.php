c77fb9f2b14b028ffc72a65c93610d347ad26682
Michael Steenbeek <m.o.steenbeek@gmail.com>
2026-04-25 15:20:43 +0200

    Bump engine version

--- src/CyndaronInfo.php
+++ src/CyndaronInfo.php
@@ -3,7 +3,7 @@ namespace Cyndaron;
 
 final class CyndaronInfo
 {
-    public const ENGINE_VERSION = '6.99.1.0';
+    public const ENGINE_VERSION = '6.99.1.1';
     public const PRODUCT_CODENAME = 'Brosella';
     public const PRODUCT_VERSION = '7.0 beta 0';
     public const PRODUCT_NAME = 'Cyndaron';
