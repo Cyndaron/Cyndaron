fd98cdbcd13c9e28b0fe507e3b2f5a1f48044638
Michael Steenbeek <m.o.steenbeek@gmail.com>
2026-06-08 20:25:23 +0200

    Also allow embedding YouTube playlists

--- src/View/Renderer/YouTubeRenderer.php
+++ src/View/Renderer/YouTubeRenderer.php
@@ -8,12 +8,15 @@ use function Safe\preg_replace;
 
 final class YouTubeRenderer implements TextPostProcessor
 {
-    private const TAG = '<div class="ratio ratio-16x9"><iframe src="https://www.youtube-nocookie.com/embed/$1" sandbox="allow-scripts allow-same-origin allow-popups allow-presentation" allowfullscreen></iframe></div>';
+    private const SINGLE_VIDEO_TAG = '<div class="ratio ratio-16x9"><iframe src="https://www.youtube-nocookie.com/embed/$1" sandbox="allow-scripts allow-same-origin allow-popups allow-presentation" allowfullscreen></iframe></div>';
+    private const PLAYLIST_TAG = '<div class="ratio ratio-16x9"><iframe src="https://www.youtube-nocookie.com/embed/videoseries?list=$1" sandbox="allow-scripts allow-same-origin allow-popups allow-presentation" allowfullscreen></iframe></div>';
 
     public function process(string $text): string
     {
         /** @var string $replaced */
-        $replaced = preg_replace('/%youtube\|([A-Za-z0-9_\-]+)%/', self::TAG, $text);
+        $replaced = preg_replace('/%youtube\|([A-Za-z0-9_\-]{20,})%/', self::PLAYLIST_TAG, $text);
+        /** @var string $replaced */
+        $replaced = preg_replace('/%youtube\|([A-Za-z0-9_\-]+)%/', self::SINGLE_VIDEO_TAG, $replaced);
         return $replaced;
     }
 }
